b810b212d80a03a972d7bdcfa9b98149220ea327
AloizioMacedo <aloiziomacedo@gmail.com>
2022-03-02 19:29:32 -0300

    Add concrete observer for GuessingProcess
    This observer intends to keep track of whether the game is over or not.

---
+++ main.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from guessing_process import GuessingObserver, GuessingProcess
+
+
+class GameStatus(GuessingObserver):
+
+    def __init__(self, guessing_process: GuessingProcess) -> None:
+        self.game_is_running = True
+        guessing_process.attach(self)
+
+    def update(self, guessing_process: GuessingProcess) -> None:
+        if guessing_process.number_of_guesses >= 5:
+            self.game_is_running = False
+            self.game_won = False
+        elif len(guessing_process.correct_indexes_guessed) == 5:
+            self.game_is_running = False
+            self.game_won = True
+
+    def trigger_end_game(self) -> None:
+        if self.game_won:
+            print("Congratulations! You guessed the word!\n")
+        else:
+            print("I'm sorry! You lost. : (\n")
+
+
