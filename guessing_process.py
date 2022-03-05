40d78a13abe94ffd8873700977741a6a1581071b
AloizioMacedo <aloiziomacedo@gmail.com>
2022-03-05 16:16:43 -0300

    Implement class WordPainter
    Its responsibility is to take care of the process of building the
    colored letters. This commit only creates the class, it doesn't integrate
    it to the algorithm yet.

--- guessing_process.py
+++ guessing_process.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from collections import defaultdict
 from random import randint
-from typing import List, Set
+from typing import Dict, List, Set
 
 words: List[str] = []
 with open("wordle-answers-alphabetical.txt") as file:
@@ -20,6 +21,64 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     BRIGHT_BLACK = '\u001b[30;1m'
+
+
+class WordPainter:
+
+    def __init__(self, correct_word: List[str], word_input: List[str]) -> None:
+        self.correct_word = correct_word
+        self.word_input = word_input
+        self._yellow_paint_available = self._get_yellow_paint_available()
+        self._canvas = [""] * len(word_input)
+
+    def _get_yellow_paint_available(self) -> Dict[str, int]:
+        correct_word_letter_count = defaultdict(
+            lambda: 0,
+            {
+                letter: self.correct_word.count(letter)
+                for letter in self.correct_word
+            }.items()
+        )
+        word_input_letter_count = {
+            letter: self.word_input.count(letter)
+            for letter in self.word_input
+        }
+        return {
+            letter: min(correct_word_letter_count[letter],
+                        word_input_letter_count[letter])
+            for letter in self.word_input
+        }
+
+    def _paint_green(self) -> None:
+        for index, letter in enumerate(self.word_input):
+            if self.word_input[index] == self.correct_word[index]:
+                self._canvas[index] = (
+                    f"{bcolors.OKGREEN}{letter.upper()}{bcolors.ENDC}"
+                    )
+                self._yellow_paint_available[letter] -= 1
+
+    def _paint_yellow(self) -> None:
+        for index, letter in enumerate(self.word_input):
+            if (letter in self.correct_word
+                    and self._yellow_paint_available[letter] > 0
+                    and self._canvas[index] == ""):
+                self._canvas[index] = (
+                    f"{bcolors.WARNING}{letter.upper()}{bcolors.ENDC}"
+                    )
+                self._yellow_paint_available[letter] -= 1
+
+    def _paint_gray(self) -> None:
+        for index, letter in enumerate(self.word_input):
+            if self._canvas[index] == "":
+                self._canvas[index] = (
+                    f"{bcolors.BRIGHT_BLACK}{letter.upper()}{bcolors.ENDC}"
+                    )
+
+    def get_painted_word(self) -> str:
+        self._paint_green()
+        self._paint_yellow()
+        self._paint_gray()
+        return "".join(self._canvas)
 
 
 class GuessingObserver(ABC):
