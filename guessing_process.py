d0f3e9c4da93f1608eb225536be0a29aaea21917
AloizioMacedo <aloiziomacedo@gmail.com>
2022-03-05 18:11:46 -0300

    Support the different game types in the algorithm
    Allows the player to select between Wordle, Dordle and Quordle.
    
    Also transfers WordPainter to game_type.py

--- guessing_process.py
+++ guessing_process.py
@@ -1,84 +1,9 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from collections import defaultdict
-from random import randint
-from typing import Dict, List, Set
+from typing import List
 
-words: List[str] = []
-with open("wordle-answers-alphabetical.txt") as file:
-    words = file.read().split("\n")
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    BRIGHT_BLACK = '\u001b[30;1m'
-
-
-class WordPainter:
-
-    def __init__(self, correct_word: List[str], word_input: List[str]) -> None:
-        self.correct_word = correct_word
-        self.word_input = word_input
-        self._yellow_paint_available = self._get_yellow_paint_available()
-        self._canvas = [""] * len(word_input)
-
-    def _get_yellow_paint_available(self) -> Dict[str, int]:
-        correct_word_letter_count = defaultdict(
-            lambda: 0,
-            {
-                letter: self.correct_word.count(letter)
-                for letter in self.correct_word
-            }.items()
-        )
-        word_input_letter_count = {
-            letter: self.word_input.count(letter)
-            for letter in self.word_input
-        }
-        return {
-            letter: min(correct_word_letter_count[letter],
-                        word_input_letter_count[letter])
-            for letter in self.word_input
-        }
-
-    def _paint_green(self) -> None:
-        for index, letter in enumerate(self.word_input):
-            if self.word_input[index] == self.correct_word[index]:
-                self._canvas[index] = (
-                    f"{bcolors.OKGREEN}{letter.upper()}{bcolors.ENDC}"
-                    )
-                self._yellow_paint_available[letter] -= 1
-
-    def _paint_yellow(self) -> None:
-        for index, letter in enumerate(self.word_input):
-            if (letter in self.correct_word
-                    and self._yellow_paint_available[letter] > 0
-                    and self._canvas[index] == ""):
-                self._canvas[index] = (
-                    f"{bcolors.WARNING}{letter.upper()}{bcolors.ENDC}"
-                    )
-                self._yellow_paint_available[letter] -= 1
-
-    def _paint_gray(self) -> None:
-        for index, letter in enumerate(self.word_input):
-            if self._canvas[index] == "":
-                self._canvas[index] = (
-                    f"{bcolors.BRIGHT_BLACK}{letter.upper()}{bcolors.ENDC}"
-                    )
-
-    def get_painted_word(self) -> str:
-        self._paint_green()
-        self._paint_yellow()
-        self._paint_gray()
-        return "".join(self._canvas)
+from game_type import GameType, get_painted_words, is_input_valid
 
 
 class GuessingObserver(ABC):
@@ -90,11 +15,17 @@ class GuessingObserver(ABC):
 
 class GuessingProcess:
 
-    def __init__(self) -> None:
-        self.correct_word = words[randint(0, len(words))]
+    def __init__(self, game_type: GameType) -> None:
+        self.game_type = game_type
+
+        self.correct_words = game_type.generate_correct_words()
+        self.were_words_guessed: List[bool] = [
+            False for word in self.correct_words
+            ]
+        self.max_guesses = game_type.get_max_guesses()
         self.number_of_guesses: int = 0
-        self.word_was_guessed = False
         self._observers: List[GuessingObserver] = []
+        print(str(self.correct_words))
 
     def attach(self, observer: GuessingObserver) -> None:
         self._observers.append(observer)
@@ -109,17 +40,21 @@ class GuessingProcess:
             word_input = input("Write your next attempt!\n")
             word_input = word_input.lower()
 
-            if word_input in words:
+            if is_input_valid(word_input):
                 word_is_valid = True
             else:
                 print("Invalid word. Please select another one.\n")
 
-        if self.correct_word == word_input:  # type: ignore
-            self.word_was_guessed = True
+        painted_words = get_painted_words(
+            word_input,  # type: ignore
+            self.correct_words,
+            self.were_words_guessed
+            )
+        print(painted_words)
 
-        word_painter = WordPainter(list(self.correct_word),
-                                   list(word_input))  # type: ignore
-        print(word_painter.get_painted_word())
+        for index, word in enumerate(self.correct_words):
+            if word_input == word:  # type: ignore
+                self.were_words_guessed[index] = True
 
         self.number_of_guesses += 1
         self._notify()
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
+from game_type import Dordle, Quordle, Wordle
 from guessing_process import GuessingObserver, GuessingProcess
+
+GAME_TYPE_OPTIONS = {
+    "wordle": Wordle,
+    "dordle": Dordle,
+    "quordle": Quordle
+}
 
 
 class GameStatus(GuessingObserver):
@@ -12,24 +19,26 @@ class GameStatus(GuessingObserver):
 
     def update(self, guessing_process: GuessingProcess) -> None:
         if (guessing_process.number_of_guesses >= 5
-                and not guessing_process.word_was_guessed):
+                and not all(guessing_process.were_words_guessed)):
             self.game_is_running = False
             self.game_won = False
-        elif guessing_process.word_was_guessed:
+        elif all(guessing_process.were_words_guessed):
             self.game_is_running = False
             self.game_won = True
 
     def trigger_end_game(self) -> None:
         if self.game_won:
-            print("Congratulations! You guessed the word!\n")
+            print("Congratulations! You guessed it right!\n")
         else:
             print("I'm sorry! You lost. : (")
             print("The correct answer was"
-                  f" {self.guessing_process.correct_word}.\n")
+                  f" {str(self.guessing_process.correct_words)}.\n")
 
 
 def main():
-    guessing_process = GuessingProcess()
+    game_type = input("Choose the game type.\n"
+                      "Options are: 'wordle', 'dordle', 'quordle'.\n")
+    guessing_process = GuessingProcess(GAME_TYPE_OPTIONS[game_type]())
     game_status = GameStatus(guessing_process)
     print("Welcome to wordle!")
     while game_status.game_is_running:
