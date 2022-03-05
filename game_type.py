3a34934e7fac236d2d3168f915b5b1d28a895147
AloizioMacedo <aloiziomacedo@gmail.com>
2022-03-05 18:06:05 -0300

    Add concrete classes of GameType
    They represent the different game modes a player can select,
    namely: Wordle, Dordle and Quordle.

--- game_type.py
+++ game_type.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from random import randint
 from typing import Dict, List
+
+WORDS: List[str] = []
+with open("wordle-answers-alphabetical.txt") as file:
+    WORDS = file.read().split("\n")
 
 
 class GameType(ABC):
@@ -13,3 +18,41 @@ class GameType(ABC):
     @abstractmethod
     def generate_correct_words(self) -> List[str]:
         pass
+
+
+class Wordle(GameType):
+
+    def get_max_guesses(self) -> int:
+        return 6
+
+    def generate_correct_words(self) -> List[str]:
+        correct_words = [
+            WORDS[randint(0, len(WORDS))]
+            ]
+        return correct_words
+
+
+class Dordle(GameType):
+
+    def get_max_guesses(self) -> int:
+        return 7
+
+    def generate_correct_words(self) -> List[str]:
+        correct_words = [
+            WORDS[randint(0, len(WORDS))]
+            for i in range(0, 2)
+            ]
+        return correct_words
+
+
+class Quordle(GameType):
+
+    def get_max_guesses(self) -> int:
+        return 9
+
+    def generate_correct_words(self) -> List[str]:
+        correct_words = [
+            WORDS[randint(0, len(WORDS))]
+            for i in range(0, 4)
+            ]
+        return correct_words
